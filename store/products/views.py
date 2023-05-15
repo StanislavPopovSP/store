6e933fc9c25aea1c6f8727fa695d6c29c198e415
Станислав <staspopov4@gmail.com>
2023-05-15 17:02:01 +0500

    Добавил вывод двух страниц, index.html и products.html

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Test title',
+        'is_promotion': False,
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 2050,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 1010,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            }
+        ]
+    }
+    return render(request, 'products/products.html', context)
