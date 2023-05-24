94f05a9eb90dfeeb1ef500be3732ff1eff7c7110
StanislavPopovSP <staspopov4@gmail.com>
2023-05-24 19:28:34 +0500

    Обновил БД, добавил модель User с полем изображение, по новому сделал минрации. Обновил фикстуры,  без учета кодировки.

--- store/users/models.py
+++ store/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser # Расширяет возможности в админ панели пользователя
+# Когда проект создается принимается решение будем ли мы работать с существующей таблицей пользователей или мы будем расширять свою
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
