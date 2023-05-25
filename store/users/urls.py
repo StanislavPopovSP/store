8c37a6ce0fe0323589731bc94073300fbafef57b
Станислав <staspopov4@gmail.com>
2023-05-25 18:37:19 +0500

    Добавил путь для страницы профиль

--- store/users/urls.py
+++ store/users/urls.py
@@ -4,4 +4,5 @@ from users.views import *
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('register/', register_user, name='register'),
+    path('profile/', profile, name='profile')
 ]
