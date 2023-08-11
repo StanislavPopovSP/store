cedab9f062986d2fb7b5cb12ff3cce304e7ea905
StanislavPopovSP <staspopov4@gmail.com>
2023-08-11 22:08:46 +0500

    Добавил обработчик products

--- store/products/views.py
+++ store/products/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """Отображает главную страницу"""
     return render(request, 'products/index.html')
+
+
+def products(request):
+    """Отображает страницу products.html"""
+    return render(request, 'products/products.html')
