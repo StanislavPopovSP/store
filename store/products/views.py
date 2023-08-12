95b78114d0ec2b0ae25687335dbf71734da65ecf
StanislavPopovSP <staspopov4@gmail.com>
2023-08-12 22:32:54 +0500

    Обновил обработчик index и products

--- store/products/views.py
+++ store/products/views.py
@@ -3,9 +3,29 @@ from django.shortcuts import render
 
 def index(request):
     """Отображает главную страницу"""
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
     """Отображает страницу products.html"""
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Синяя куртка The North Face',
+                'price': 23725,
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
+            }
+        ]
+    }
+    return render(request, 'products/products.html', context)
