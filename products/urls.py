725b749dfc86b73287bb98f78d93ee6f959f322f
StanislavPopovSP <staspopov4@gmail.com>
2023-08-21 02:02:14 +0500

    Добавил путь для корзины

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from products.views import products
+from products.views import products, basket_add
 
 
 app_name = 'products'
 urlpatterns = [
-     path('', products, name='products')
+     path('', products, name='products'),
+     path('baskets/add/<int:product_id>/', basket_add, name='basket_add')  # ..products/baskets/add/<products_id>/
 ]
