9d0ae7f8ee7f6eb5001b1a5be6e28f5e3465850f
StanislavPopovSP <staspopov4@gmail.com>
2023-08-23 17:13:10 +0500

    Добавил путь для выделенной категории

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from products.views import products, basket_add, basket_remove
 app_name = 'products'
 urlpatterns = [
      path('', products, name='products'),
+     path('category/<int:category_id>/', products, name='category'),
      path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),  # ..products/baskets/add/<products_id>/
-     path('baskets/remove/<int:basket_id>', basket_remove, name='basket_remove')
+     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove')
 ]
