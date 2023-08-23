60213ae5950b8db7a593812d3ebddffbcd3728d1
StanislavPopovSP <staspopov4@gmail.com>
2023-08-23 14:52:19 +0500

    Добавил путь для удаления товара из корзины

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from products.views import products, basket_add
+from products.views import products, basket_add, basket_remove
 
 
 app_name = 'products'
 urlpatterns = [
      path('', products, name='products'),
-     path('baskets/add/<int:product_id>/', basket_add, name='basket_add')  # ..products/baskets/add/<products_id>/
+     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),  # ..products/baskets/add/<products_id>/
+     path('baskets/remove/<int:basket_id>', basket_remove, name='basket_remove')
 ]
