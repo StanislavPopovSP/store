3eae04af584e7a3c64b09cfc8f1c51c7b9706ccd
StanislavPopovSP <staspopov4@gmail.com>
2023-08-25 20:38:42 +0500

    Привязал корзину к пользователю в админке

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from users.models import User
+from products.admin import BasketAdmin
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    inlines = (BasketAdmin,) # BasketAdmin - будем отображатьв админке пользователя, сразу когда будем заходить на страницу пользователя сразу будем видеть какие товары у него были добавлены в корзину. (Корзину прикрутили для пользователя)
