0d27398bf9b6465503fec1b34b20f52198d7a34e
StanislavPopovSP <staspopov4@gmail.com>
2023-08-27 18:18:16 +0500

    Обновил пути с применением представлений

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-from products.views import products, basket_add, basket_remove
+from products.views import ProductsListView, basket_add, basket_remove
 
 
 app_name = 'products'
+
+# С применением методов
+# urlpatterns = [
+#      path('', products, name='products'),
+#      path('category/<int:category_id>/', products, name='category'),
+#      path('page/<int:page_number>', products, name='paginator'),
+#      path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),  # ..products/baskets/add/<products_id>/
+#      path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove')
+# ]
+
+# С применением представлений
 urlpatterns = [
-     path('', products, name='products'),
-     path('category/<int:category_id>/', products, name='category'),
-     path('page/<int:page_number>', products, name='paginator'),
+     path('', ProductsListView.as_view(), name='products'),
+     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
+     path('page/<int:page>', ProductsListView.as_view(), name='paginator'),
      path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),  # ..products/baskets/add/<products_id>/
      path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove')
 ]
