84ba4135771e595270348af1bf9faf91a3eeb4b2
StanislavPopovSP <staspopov4@gmail.com>
2023-08-27 18:16:32 +0500

    Заменил обработчик products на представление ProductsListView

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from products.models import Product, ProductCategory, Basket
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 
 class IndexView(TemplateView):
@@ -23,25 +25,42 @@ class IndexView(TemplateView):
 #     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None, page_number=1):
+class ProductsListView(ListView):
     """Отображает страницу products.html"""
-    # if category_id:
-    #     # category = ProductCategory.objects.get(id=category_id)
-    #     products = Product.objects.filter(category=category_id)
-    # else:
-    #     products = Product.objects.all()
-    # Через тернарный оператор
-    products = Product.objects.filter(category=category_id) if category_id else Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products, per_page)  # per_page - сколько товаров нужно отображать на странице
-    products_paginator = paginator.page(page_number) # page() - номер страницы товара который нужно отобразить
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
-    context = {
-            'title': 'Store - Каталог',
-            'products': products_paginator,
-            'categories': ProductCategory.objects.all()
-        }
-    return render(request, 'products/products.html', context)
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()  # queryset - это уже сформированный список объектов Product.objects.all()
+        category_id = self.kwargs.get('category_id')  # в self.kwargs хранятся дополнительные приходящие параметры
+        return queryset.filter(category=category_id) if category_id else queryset
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+# def products(request, category_id=None, page_number=1):
+#     """Отображает страницу products.html"""
+#     # if category_id:
+#     #     # category = ProductCategory.objects.get(id=category_id)
+#     #     products = Product.objects.filter(category=category_id)
+#     # else:
+#     #     products = Product.objects.all()
+#     # Через тернарный оператор
+#     products = Product.objects.filter(category=category_id) if category_id else Product.objects.all()
+#     per_page = 3
+#     paginator = Paginator(products, per_page)  # per_page - сколько товаров нужно отображать на странице
+#     products_paginator = paginator.page(page_number) # page() - номер страницы товара который нужно отобразить
+
+#     context = {
+#             'title': 'Store - Каталог',
+#             'products': products_paginator,
+#             'categories': ProductCategory.objects.all()
+#         }
+#     return render(request, 'products/products.html', context)
 
 # Обработчик событий
 @login_required
