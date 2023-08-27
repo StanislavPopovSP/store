920c913f630535b20ef341513f4bede9c10ee96b
StanislavPopovSP <staspopov4@gmail.com>
2023-08-27 23:03:00 +0500

    Обновил обработку путей c помощью представлений login, logout

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from users.views import login, logout
-from users.views import UserRegistrationView, UpdateProfileView
+from users.views import UserRegistrationView, UpdateProfileView, UserLoginView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
 app_name = 'users'
 
@@ -15,8 +15,8 @@ app_name = 'users'
 
 # Через представления
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('registration/ ', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>/', login_required(UpdateProfileView.as_view()), name='profile'),
-    path('logout/', logout, name='logout')
+    path('logout/', login_required(LogoutView.as_view()), name='logout')
 ]
