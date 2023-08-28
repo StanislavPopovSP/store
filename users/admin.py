b54ce89acd2dd36c6af13b3a99d4c2d1b9438f91
StanislavPopovSP <staspopov4@gmail.com>
2023-08-28 23:56:15 +0500

    Добавил подключение админ панели для модели EmailVerification

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from users.models import User
+from users.models import User, EmailVerification
 from products.admin import BasketAdmin
 
 
@@ -7,3 +7,10 @@ from products.admin import BasketAdmin
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
     inlines = (BasketAdmin,) # BasketAdmin - будем отображатьв админке пользователя, сразу когда будем заходить на страницу пользователя сразу будем видеть какие товары у него были добавлены в корзину. (Корзину прикрутили для пользователя)
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'user', 'expiration')
+    fields = ('code', 'user', 'expiration', 'created')
+    readonly_fields = ('created',)
