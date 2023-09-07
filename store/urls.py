4c45c5a85fc7bb1f3f2dc3bde1b1a07e4bc7e839
StanislavPopovSP <staspopov4@gmail.com>
2023-09-07 21:44:16 +0500

    Добавил путь для авторизаций через соц сети

--- store/urls.py
+++ store/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),  # namespace - для того что бы могли в шаблонах обращаться
-    path('users/', include('users.urls', namespace='users'))
+    path('users/', include('users.urls', namespace='users')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
