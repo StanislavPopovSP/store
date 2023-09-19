7d13f8911a8bed33625f63bc0509509b7810f0ef
StanislavPopovSP <staspopov4@gmail.com>
2023-09-19 22:17:51 +0500

    Добавил обработчик OrderCreateView оформление заказа

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from common.views import TitleMixin
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class OrderCreateView(TitleMixin, TemplateView):
+    """Оформление заказа"""
+    template_name = 'orders/order-create.html'
+    title = 'Store - Оформление заказа'
